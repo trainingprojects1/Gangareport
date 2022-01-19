a45817ad3a282d0081556fa0af096f35df5aac9e
DESKTOP-VTPQ1BR\gtama <tamangganga807@gmail.com>
2022-01-18 22:58:02 -0500

    first commit

---
+++ src/helloWorld/HelloWord.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		//To print anything in Java application
+		System.out.println("Hello World!! This is my first Java application");
+		System.out.println("This is good day");
+
+		System.out.println("my name is Ganga Tamang");
+
+	}
+
+}
---
+++ src/main/java/com/java/spr/App.java
@@ -0,0 +1,9 @@
+package main.java.com.java.spr;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
