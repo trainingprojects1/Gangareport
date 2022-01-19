79fa06f86399553864799c8957610fe4f6853e9f
DESKTOP-VTPQ1BR\gtama <tamangganga807@gmail.com>
2022-01-18 23:14:35 -0500

    first commit

--- src/helloWorld/HelloWord.java
+++ src/helloWorld/HelloWord.java
@@ -5,7 +5,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		//To print anything in Java application
 		System.out.println("Hello World!! This is my first Java application");
-		System.out.println("This is good day");
+		System.out.println("Hello my name is Ganga");
 
 		System.out.println("my name is Ganga Tamang");
 
